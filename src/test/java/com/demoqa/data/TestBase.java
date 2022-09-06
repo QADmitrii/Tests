367441eb5c9a88d4436400713c20c88e4f1fb745
Tillerg <mrgish@yandex.ru>
2022-09-06 15:20:13 +0300

    remote

--- src/test/java/com/demoqa/data/TestBase.java
+++ src/test/java/com/demoqa/data/TestBase.java
@@ -1,15 +1,20 @@
 package com.demoqa.data;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.page.StudentFormPageObject;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
+
 
     StudentFormPageObject pageObjectForm = new StudentFormPageObject();
 
     @BeforeAll
     static void config() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserPosition = "0x0";
         Configuration.browserSize = "1920x1080";
