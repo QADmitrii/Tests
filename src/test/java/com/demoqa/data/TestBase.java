822b41aa02f7cbb4279d8097f5065b0759a9e72d
Tillerg <mrgish@yandex.ru>
2022-09-06 15:30:24 +0300

    attach

--- src/test/java/com/demoqa/data/TestBase.java
+++ src/test/java/com/demoqa/data/TestBase.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.page.StudentFormPageObject;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -19,5 +20,13 @@ public class TestBase {
         Configuration.browserPosition = "0x0";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screen");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+
     }
 }
