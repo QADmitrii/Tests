428aebbc2e2496de26ac1b887c2aba9ed759cd4a
Tillerg <mrgish@yandex.ru>
2022-09-06 12:41:57 +0300

    новый коммент

--- src/test/java/com/demoqa/StudentFormTestFakerData.java
+++ src/test/java/com/demoqa/StudentFormTestFakerData.java
@@ -10,7 +10,7 @@ import static java.lang.String.format;
 
 public class StudentFormTestFakerData extends TestBase {
     StudentFormPageObject pageObjectForm = new StudentFormPageObject();
-    //за веру и народ! Работай на дженкинс
+    //за веру и народ! Работай на дженкинс, плиз
     @Test
     void positiveTestFillForm() {
         pageObjectForm.openPage()
