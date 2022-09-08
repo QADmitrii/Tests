5e12b1f3b37be700dfa43d7cefad3c499430e6e8
Tillerg <mrgish@yandex.ru>
2022-09-08 19:18:38 +0300

    Configuration.timeout = 10_000;

--- src/test/java/com/demoqa/data/TestBase.java
+++ src/test/java/com/demoqa/data/TestBase.java
@@ -25,7 +25,7 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-        
+        Configuration.timeout = 10_000;
         Configuration.browser = System.getProperty("browser_name", "chrome");
         Configuration.browserVersion = System.getProperty("browser_version", "105.0");
         Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
